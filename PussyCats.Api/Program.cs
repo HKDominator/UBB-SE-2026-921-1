ceefe96b0fac963f46856bc5d8c92e7278d35847
berintanemanuel <natiberintan3@gmail.com>
2026-05-06 15:42:13 +0300

    Add repos for dependency injection

--- PussyCats.Api/Program.cs
+++ PussyCats.Api/Program.cs
@@ -1,5 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using PussyCats.Library.Persistence;
+using PussyCats.Library.Repositories.Companies;
+using PussyCats.Library.Repositories.Documents;
+using PussyCats.Library.Repositories.Jobs;
+using PussyCats.Library.Repositories.Matches;
+using PussyCats.Library.Repositories.PersonalityTests;
+using PussyCats.Library.Repositories.Recommendations;
+using PussyCats.Library.Repositories.Skills;
+using PussyCats.Library.Repositories.SkillTests;
+using PussyCats.Library.Repositories.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +20,21 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<PussyCatsDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PussyCatsDb")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<IPersonalityTestRepository, PersonalityTestRepository>();
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<IRecommendationRepository, RecommendationRepository>();
+builder.Services.AddScoped<IJobSkillRepository, JobSkillRepository>();
+builder.Services.AddScoped<ISkillGroupRepository, SkillGroupRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+builder.Services.AddScoped<IUserSkillRepository, UserSkillRepository>();
+builder.Services.AddScoped<ISkillTestRepository, SkillTestRepository>();
+
 
 var app = builder.Build();
 

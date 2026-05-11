04612697c3f2807162afecd3ac1bff46075bfacf
aldenisa <d3157190@gmail.com>
2026-05-11 22:25:11 +0300

    deleted chatId from Message

--- PussyCats.Library/Domain/Message.cs
+++ PussyCats.Library/Domain/Message.cs
@@ -5,7 +5,6 @@ namespace PussyCats.Library.Domain;
 public class Message
 {
     public int MessageId { get; set; }
-    public int ChatId { get; set; }
     public Chat Chat { get; set; }
     public int SenderId { get; set; }
     public User Sender { get; set; }

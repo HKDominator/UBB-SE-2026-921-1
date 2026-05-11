15b08b575a2720b7b89e6aef5b8638c116772e8b
aldenisa <d3157190@gmail.com>
2026-05-12 02:17:07 +0300

    commented ids, they are referenced only in tests (to be done for test refactoring)

--- PussyCats.Library/Domain/UserSkill.cs
+++ PussyCats.Library/Domain/UserSkill.cs
@@ -7,7 +7,7 @@ public class UserSkill
     //public int UserId { get; set; }
     [JsonIgnore] public User User { get; set; } = null!;
 
-   // public int SkillId { get; set; }
+    //public int SkillId { get; set; }
     public Skill Skill { get; set; } = null!;
 
     public int Score { get; set; }
